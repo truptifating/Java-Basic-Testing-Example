9a998a7fa7f67122e89eb5045f6e92b4517fe8db
truptifating <truptifating@gmail.com>
2024-12-29 04:48:18 +0530

    modified few changes

--- src/com/numbers/prog/Test.java
+++ src/com/numbers/prog/Test.java
@@ -1,6 +1,7 @@
 package com.numbers.prog;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Test {
@@ -8,21 +9,20 @@ public class Test {
 
 	public static void main(String[] args) 
 	{
-		 int row=6,i,j;
-		 
-		 for(i=0;i<row;i++)
-		 {
-			 for(j=2*(row-i);j>1;j--)
-			 {
-				 System.out.print(" ");
-			 }
-			 for(j=0;j<=i;j++)
-			 {
-				 System.out.print("* ");
-			 }
-			 System.out.println();
-		 }
+		HashMap<Integer,String> hm= new HashMap<Integer,String>();
+		hm.put(1,"trupti");
+		hm.put(2, "Ram");
+		hm.put(3,"Vishnu");
+		System.out.println(hm);
+		
+		for(Integer i:hm.keySet())
+		{
+			System.out.println(i);
+		}
+		for(String s:hm.values())
+		{
+			System.out.println(s);
+		}
 	}
 
 }
-///classname.main(new String[0])
