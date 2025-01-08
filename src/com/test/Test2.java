3491b62757f766b1ab42ef448b2e387d69cb1ea1
truptifating <66105555+truptifating@users.noreply.github.com>
2025-01-08 11:13:07 +0530

    Update Test2.java
    updated the branch

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -18,19 +18,7 @@ class Test2 {
 
 	public static void main(String[] args) 
 	{
-		int row=6,i,j;
-		for(i=0;i<row;i++)
-		{
-			for(j=2*(row-i);j>1;j--)
-			{
-				System.out.print(" ");
-			}
-			for(j=0;j<=i;j++)
-			{
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
+		
 		
 		
 		
